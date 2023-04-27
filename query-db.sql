28a0eba367d46a29cf7afa37dc7731287abfc932
kotbarmaglot <allonewage@gmail.com>
2023-04-27 11:02:26 +0300

    add create_table/fix query

--- query-db.sql
+++ query-db.sql
@@ -1,6 +1,6 @@
 /* Показать работников у которых нет почты или почта 
 не в корпоративном домене (домен dualbootpartners.com) */
-SELECT * FROM Employees WHERE Email = '' OR Email NOT LIKE '%dualbootpartners.com';
+SELECT * FROM Employees WHERE IS NULL OR Email NOT LIKE '%dualbootpartners.com';
 
 --Получить список работников нанятых в последние 30 дней
 SELECT * FROM Employees WHERE Hire_date > (NOW() - INTERVAL '30 days');
